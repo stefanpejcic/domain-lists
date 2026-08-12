63b8fec8cca4de91ad26409760da0edff6c7ca1a
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-08-12 12:59:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
-cd /home
+#!/usr/bin/env bash
+set -e
+
 apt install -y python3-full python3-venv python3-pip
-python3 -m venv venv
+
+python3 -m venv /home/venv
+
 /home/venv/bin/pip install --upgrade pip
 /home/venv/bin/pip install flask
+
 /home/venv/bin/python -c "import flask; print(flask.__version__)"
 
-/home/venv/bin/python app.py
+#test /home/venv/bin/python app.py
+
+# Add daily cron job at 03:05
+CRON_JOB="5 3 * * * bash /home/daily.sh"
+
+(crontab -l 2>/dev/null | grep -F "$CRON_JOB") || (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+
+echo "DONE"
