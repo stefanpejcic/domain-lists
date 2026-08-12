a6ac86e54bb1054bca984cf489dba4169ab852cd
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-08-12 12:56:01 +0200

    Create daily.sh

---
+++ daily.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 1. download zone files
+/home/venv/bin/python /home/download_zones.py
+
+# 2. compare with previous date and geenerate lists
+/home/venv/bin/python /home/process_zones.py
+
+# 3. download tld info
+/home/venv/bin/python /home/tld_scrapper.py
+
+# 4. cleanup (~9.6G)
+rm -rf /home/downloads/
+
+# 5. restart app
+pkill -f '/home/venv/bin/python app.py' 2>/dev/null; /home/venv/bin/python app.py &
